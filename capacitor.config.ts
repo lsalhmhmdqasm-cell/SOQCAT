82b74c498679be9ec45da88fc926f3f629160f52
nasrnajeeb23-spec <242033753+nasrnajeeb23-spec@users.noreply.github.com>
2025-12-22 23:13:06 +0300

    تحسينات  منصة التطبيقات

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.qatshop.sanaa',
-  appName: 'سوق صنعاء',
+  appId: 'com.qatshop.binabboud',
+  appName: 'سوق بن عبود',
   webDir: 'dist'
 };
 
